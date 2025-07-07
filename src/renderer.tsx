d02859faaa4f8a35557ce4cebbf80ed2c30b8b4b
khalil-cheddadi <khalil3cheddadi@gmail.com>
2025-07-07 18:04:05 +0100

    switched to vite and made tailwindcss work!!!!!

---
+++ src/renderer.tsx
@@ -0,0 +1,8 @@
+import "./index.css";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <h1 className="text-gray-200 bg-gray-800 font-bold rounded-3xl p-4 m-4">
+    Hello from React + Vite + Tailwindcss!
+  </h1>,
+);
