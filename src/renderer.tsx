46b07bcd05d57ee92471f1f180dd56253c3f8aa3
0xcyborg <0xcyborgx@gmail.com>
2025-07-09 16:00:30 +0100

    HashRouter works with Electron in production unlike BrowserRouter

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,13 +1,13 @@
 import "./index.css";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "./lib/contexts/ThemeContext";
 import App from "./pages/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ThemeProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ThemeProvider>,
 );
