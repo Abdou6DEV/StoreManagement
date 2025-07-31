e544fb1b5a58f3c8ed8db62e7fdd701af88e3d27
0xcyborg <0xcyborgx@gmail.com>
2025-07-31 23:17:04 +0100

    Improving search sales spacing

--- src/pages/cashier/components/historySales.tsx
+++ src/pages/cashier/components/historySales.tsx
@@ -162,7 +162,7 @@ const HistorySales: React.FC<HistorySalesProps> = ({ onSaleSelect }) => {
   return (
     <div className="h-full flex flex-col">
       {/* Search Bar */}
-      <div className="p-3 border-b border-border bg-muted/20">
+      <div className="bg-muted/20">
         <div className="relative">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-muted-foreground" />
           <Input
